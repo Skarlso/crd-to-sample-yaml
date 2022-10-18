a3c6718d509175d0364f7386afa0caa6ff70dc2d
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-10-18 23:01:35 +0200

    Skeleton of parsing a CRD

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Usage: [yaml-from-crd] <path-to-crd>")
+		os.Exit(1)
+	}
+	path := args[1]
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		printAndQuit("file under '%s' does not exist\n", path)
+	}
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		printAndQuit("failed to read file: ", err)
+	}
+
+	crd := &v1beta1.CustomResourceDefinition{}
+	if err := yaml.Unmarshal(content, crd); err != nil {
+		printAndQuit("failed to unmarshal into custom resource definition")
+	}
+
+	// TODO: Each version should have its own file.
+	dir := filepath.Dir(path)
+	outputLocation := filepath.Join(dir, "output.yaml")
+	outputFile, err := os.Create(outputLocation)
+	if err != nil {
+		printAndQuit("failed to create file at: '%s': %v", outputLocation, err)
+	}
+	defer outputFile.Close()
+	//
+	//fmt.Println(crd.Spec.Versions[0].Schema.OpenAPIV3Schema.Properties)
+
+	// parseHeader -- parses apiVersion, kind, metadata
+	parseProperties(crd.Spec.Versions[0].Schema.OpenAPIV3Schema.Properties, outputFile, 0)
+}
+
+func parseProperties(properties map[string]v1beta1.JSONSchemaProps, file *os.File, indent int) {
+	for k, v := range properties {
+		if _, err := file.WriteString(fmt.Sprintf("%s%s:\n", strings.Repeat(" ", indent), k)); err != nil {
+			printAndQuit("failed to write k to file '%s'", k)
+		}
+		if len(v.Properties) > 0 {
+			parseProperties(v.Properties, file, indent+2)
+			continue
+		}
+	}
+}
+
+func printAndQuit(msg string, args ...any) {
+	fmt.Printf(msg, args...)
+	os.Exit(1)
+}
