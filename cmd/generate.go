c6fdfb5960f3915a78781bbd37e1a36cba7b5633
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-05-09 10:52:26 +0200

    feat: generate an HTML based output for a given CRD when format is defined

--- cmd/generate.go
+++ cmd/generate.go
@@ -16,6 +16,11 @@ import (
 	"github.com/Skarlso/crd-to-sample-yaml/pkg/fetcher"
 )
 
+const (
+	FormatHTML = "html"
+	FormatYAML = "yaml"
+)
+
 var (
 	// generateCmd is root for various `generate ...` commands.
 	generateCmd = &cobra.Command{
@@ -27,6 +32,7 @@ var (
 	fileLocation string
 	url          string
 	output       string
+	format       string
 	stdOut       bool
 	comments     bool
 )
@@ -38,6 +44,7 @@ func init() {
 	f.StringVarP(&fileLocation, "crd", "c", "", "The CRD file to generate a yaml from.")
 	f.StringVarP(&url, "url", "u", "", "If provided, will use this URL to fetch CRD YAML content from.")
 	f.StringVarP(&output, "output", "o", "", "The location of the output file. Default is next to the CRD.")
+	f.StringVarP(&format, "format", "f", FormatYAML, "The format in which to output. Default is YAML. Options are: yaml, html.")
 	f.BoolVarP(&stdOut, "stdout", "s", false, "If set, it will output the generated content to stdout")
 	f.BoolVarP(&comments, "comments", "m", false, "If set, it will add descriptions as comments to each line where available")
 }
@@ -68,18 +75,27 @@ func runGenerate(_ *cobra.Command, _ []string) error {
 	if err := yaml.Unmarshal(content, crd); err != nil {
 		return errors.New("failed to unmarshal into custom resource definition")
 	}
+
 	if stdOut {
 		w = os.Stdout
 	} else {
 		if output == "" {
 			output = filepath.Dir(fileLocation)
 		}
-		outputLocation := filepath.Join(output, crd.Name+"_sample.yaml")
+		outputLocation := filepath.Join(output, crd.Name+"_sample."+format)
 		outputFile, err := os.Create(outputLocation)
 		if err != nil {
 			return fmt.Errorf("failed to create file at: '%s': %w", outputLocation, err)
 		}
 		w = outputFile
+	}
+
+	if format == FormatHTML {
+		if err := pkg.LoadTemplates(); err != nil {
+			return fmt.Errorf("failed to load templates: %w", err)
+		}
+
+		return pkg.RenderContent(w, content, comments)
 	}
 
 	return pkg.Generate(crd, w, comments)
