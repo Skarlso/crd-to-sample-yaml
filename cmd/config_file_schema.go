5d3c6213d39a2574f2615fdcdcf12c1723797aa2
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2025-01-05 10:54:14 +0100

    feat: extend the configuration file with the other providers
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

---
+++ cmd/config_file_schema.go
@@ -0,0 +1,33 @@
+package cmd
+
+type URLs struct {
+	URL      string `json:"url"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	Token    string `json:"token,omitempty"`
+}
+
+type GITUrls struct {
+	URL         string `json:"url"`
+	Username    string `json:"username,omitempty"`
+	Password    string `json:"password,omitempty"`
+	Token       string `json:"token,omitempty"`
+	Tag         string `json:"tag,omitempty"`
+	PrivateKey  string `json:"privateKey,omitempty"`
+	UseSSHAgent bool   `json:"useSSHAgent,omitempty"`
+}
+
+// APIGroups defines groups by which grouping will happen in the resulting HTML output.
+type APIGroups struct {
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	Files       []string  `json:"files,omitempty"`
+	Folders     []string  `json:"folders,omitempty"`
+	URLs        []URLs    `json:"urls,omitempty"`
+	GitURLs     []GITUrls `json:"gitUrls,omitempty"`
+}
+
+// RenderConfig defines a configuration for the resulting rendered HTML content.
+type RenderConfig struct {
+	APIGroups []APIGroups `json:"apiGroups"`
+}
